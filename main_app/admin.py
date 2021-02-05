ce6825e2dcb5480c9a72240acf0ad70ce4f0fb1c
nistalb <brent.daddy@gmail.com>
2021-02-04 17:43:10 -0800

    adds defaults to positiveintegerfields in models.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Profile, Equipment, Task, Tool, Consumables, Maint_Record
 
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Equipment)
+admin.site.register(Task)
+admin.site.register(Tool)
+admin.site.register(Consumables)
+admin.site.register(Maint_Record)
--- main_app/models.py
+++ main_app/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     location = models.CharField(max_length=100, blank=True)
-    hourly_rate = models.PositiveIntegerField()
+    hourly_rate = models.PositiveIntegerField(blank=True, default=0)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -16,12 +16,12 @@ class Profile(models.Model):
 class Equipment(models.Model):
     model = models.CharField(max_length=100, blank=False)
     make = models.CharField(max_length=100, blank=True)
-    mfg_year = models.PositiveIntegerField()
+    mfg_year = models.PositiveIntegerField(blank=True, default=0)
     description = models.TextField(blank=True)
-    mileage = models.PositiveIntegerField()
-    hours = models.PositiveIntegerField()
+    mileage = models.PositiveIntegerField(blank=True, default=0)
+    hours = models.PositiveIntegerField(blank=True, default=0)
     img_url = models.URLField(max_length=200, blank=True)
-    cost = models.PositiveIntegerField()
+    cost = models.PositiveIntegerField(blank=True, default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -31,8 +31,8 @@ class Equipment(models.Model):
 class Task(models.Model):
     task_name = models.CharField(max_length=100, blank=False)
     description = models.TextField(blank=True)
-    interval = models.PositiveIntegerField()
-    duration = models.PositiveIntegerField()
+    interval = models.PositiveIntegerField(blank=True, default=0)
+    duration = models.PositiveIntegerField(blank=True, default=0)
     instructions = models.TextField(blank=True)
     video = models.URLField(max_length=200, blank=True)
     equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
@@ -55,7 +55,7 @@ class Consumables(models.Model):
     description = models.TextField(blank=True)
     part_number = models.CharField(max_length=100, blank=True)
     source = models.CharField(max_length=100, blank=True)
-    cost = models.PositiveIntegerField() 
+    cost = models.PositiveIntegerField(blank=True, default=0) 
     task = models.ManyToManyField(Task)
 
     def __str__(self):
@@ -64,8 +64,8 @@ class Consumables(models.Model):
 
 class Maint_Record(models.Model):
     date = models.DateField(auto_now=True)
-    mileage = models.PositiveIntegerField() 
-    hours = models.PositiveIntegerField() 
+    mileage = models.PositiveIntegerField(blank=True, default=0) 
+    hours = models.PositiveIntegerField(blank=True, default=0) 
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
 
