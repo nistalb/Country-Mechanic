57d5830cfe9ab756f29f9cdc7ec0093a49936240
nistalb <brent.daddy@gmail.com>
2021-02-26 08:44:39 -0800

    hides secret key

--- country_mechanic/settings.py
+++ country_mechanic/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qe%onkenc4f*1**442qsj^71z236jd92vkn+_1i)ktmhzig!2w'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
